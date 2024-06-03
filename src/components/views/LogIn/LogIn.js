6ef8c4380473f47c225daf331c788a09b361fec9
Lucía A. <u20211a452@upc.edu.pe>
2024-06-03 00:06:14 -0500

    Add: Styles
    Co-Authored-By: L.T. <104176051+NotLT42@users.noreply.github.com>
    Co-Authored-By: Luis Mario Gonzales Anaya <u20201c585@upc.edu.pe>
    Co-Authored-By: Luis Alberto Siancas Retegui <112042262+DahuX04@users.noreply.github.com>

--- src/components/views/LogIn/LogIn.js
+++ src/components/views/LogIn/LogIn.js
@@ -1,32 +1,47 @@
 import TextInput from "../../elements/Elements";
+import loginPicture from '../../assets/login_picture.png';
 import './LogIn.css'
 function LogIn() {
     return (
         <div className = "login-container">
 
             <div className = "left-container">
-
+                <img className="login-picture" src={loginPicture} alt=""/>
                 
             </div>
 
             <div className = "right-container">
-                <div className = "title-container"></div>
 
                 <form className = "login-form">
-                    <p className = "textinput-title">
-                        Usuario
-                    </p>
-                    <TextInput
-                        type = {'text'}
-                        placeholder = {'Ingrese su usuario'}
-                    ></TextInput>
-                    <p className = "textinput-title">
-                        Contraseña
-                    </p>
-                    <TextInput
-                        type = {'text'}
-                        placeholder = {'Ingrese su contraseña'}
-                    ></TextInput>
+                    <div className = "title-container">
+                        <h1 className = "login-title">
+                            Inicia Sesión
+                        </h1>
+                        <div className="register-redirect">
+                        <p>¿No tienes una cuenta?</p>
+                        <a className="link" href="/">Registrate</a>
+                    </div>
+                    </div>
+
+                    <div className = "login-group">
+                        <p className = "textinput-title">
+                            Usuario
+                        </p>
+                        <TextInput
+                            type = {'text'}
+                            placeholder = {'Ingrese su usuario'}
+                        />
+                    </div>
+                    <div className = "login-group">
+                        <p className = "textinput-title">
+                            Contraseña
+                        </p>
+                        <TextInput
+                            type = {'text'}
+                            placeholder = {'Ingrese su contraseña'}
+                        />
+                    </div>
+                    
                 </form>
                 
             </div>
