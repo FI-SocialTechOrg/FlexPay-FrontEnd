3d6c9f87a273f2691013c6a34c2a1031234a5fdf
Lucía A. <u20211a452@upc.edu.pe>
2024-06-19 15:41:56 -0500

    Add: route validations
    Added route validations
    
    Co-Authored-By: Luis Alberto Trujillo Lopez <104176051+NotLT42@users.noreply.github.com>
    Co-Authored-By: Luis Mario Gonzales Anaya <u20201c585@upc.edu.pe>
    Co-Authored-By: Luis Alberto Siancas Retegui <112042262+DahuX04@users.noreply.github.com>

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
+import { useState } from 'react';
 import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
-import './App.css';
-import { AnimatePresence } from "framer-motion";
 import Auth from './components/views/authentication/Auth';
 import Welcome from './components/views/configuration/Welcome';
 import ClientHomeView from './components/views/home/ClientHomeView';
-import 'react-toastify/dist/ReactToastify.css';
 import StoreHomeView from './components/views/home/StoreHomeView';
+import { AnimatePresence } from "framer-motion";
+import './App.css';
+import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
+  const [user, setUser] = useState({ isLoggedIn: false, type: null });
+
+  const PrivateRoute = ({ element: Component, isLoggedIn, allowedType, ...rest }) => {
+    return isLoggedIn && user.type === allowedType ? (
+      <Component {...rest} />
+    ) : (
+      <Navigate to="/auth" />
+    );
+  };
 
   return (
     <Router>
@@ -17,10 +27,10 @@ function App() {
       >
           <Routes>
             <Route exact path="/" element={<Navigate to="/auth" />} />
-            <Route exact path="/auth/*" element={<Auth />} />
-            <Route path="/welcome" element={<Welcome />} />
-            <Route path="/client/*" element={<ClientHomeView />} />
-            <Route path="/store/*" element={<StoreHomeView />} />
+            <Route exact path="/auth/*" element={<Auth setUser={setUser} />} />
+            <Route path="/welcome" element={<PrivateRoute element={Welcome} isLoggedIn={user.isLoggedIn} allowedType="client" />} />
+            <Route path="/client/*" element={<PrivateRoute element={ClientHomeView} isLoggedIn={user.isLoggedIn} allowedType="client" />} />
+            <Route path="/store/*" element={<PrivateRoute element={StoreHomeView} isLoggedIn={user.isLoggedIn} allowedType="store" />} />
           </Routes>
       </AnimatePresence>
     </Router>
